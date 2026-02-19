8190048fafb24386de3e042f7598684521ee6a15
bousii <owen.a.giles@gmail.com>
2026-02-19 16:53:48 -0600

    Add filetree (use \) and dashbaord

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,48 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
+    config = function()
+      require('dashboard').setup {
+        config = {
+          week_header = {
+            enable = true,
+          },
+          packages = { enable = true }, -- show how many plugins neovim loaded
+          -- limit how many projects list, action when you press key or enter it will run this action.
+          -- action can be a function type, e.g.
+          -- action = func(path) vim.cmd('Telescope find_files cwd=' .. path) end
+          mru = { enable = true, limit = 10, icon = '🚀', label = '', cwd_only = false },
+          footer = {}, -- footer
+          shortcut = {
+            { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+            {
+              icon = ' ',
+              icon_hl = '@variable',
+              desc = 'Files',
+              group = 'Label',
+              action = 'Telescope find_files',
+              key = 'f',
+            },
+            {
+              desc = ' Apps',
+              group = 'DiagnosticHint',
+              action = 'Telescope app',
+              key = 'a',
+            },
+            {
+              desc = ' dotfiles',
+              group = 'Number',
+              action = 'Telescope dotfiles',
+              key = 'd',
+            },
+          },
+        },
+      }
+    end,
+    dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+  },
+}
